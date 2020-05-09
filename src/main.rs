8e4d668e04bbf28669fa9d78eca1ca345b48bbac
Justin Miller <justinrmiller@gmail.com>
2020-05-09 16:48:15 -0700

    It is finally working

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,16 @@ use std::cmp::Ordering;
 use std::thread;
 use ordered_float::OrderedFloat;
 
+use evmap::{ReadHandle, WriteHandle};
+
 use rand::{thread_rng, Rng};
 use rand::distributions::Alphanumeric;
 
 type DashMap<K, V> = DMF<K, V, FxBuildHasher>;
 
-const SIZE: u32 = 8 * 1024 * 1024;
-const HEAP_CAPACITY: usize = 5;
-const NTHREADS: u32 = 4;
-//const SIZE: u64 = 10;
+const SIZE: u32 = 32 * 1024 * 1024;
+const HEAP_CAPACITY: usize = 10;
+const NTHREADS: u32 = 16;
 
 #[derive(Clone, Hash)]
 struct State {
@@ -51,8 +52,6 @@ impl PartialOrd for State {
     }
 }
 
-use evmap::ReadHandle;
-use evmap::WriteHandle;
 fn generate_ev_map(heap_number: u32, size: u32) -> (ReadHandle<String, Box<State>>, WriteHandle<String, Box<State>>) {
     println!("Generating heap number: {} with size {}", heap_number, size);
     let mut rng = rand::thread_rng();
@@ -76,25 +75,27 @@ fn generate_ev_map(heap_number: u32, size: u32) -> (ReadHandle<String, Box<State
 fn gen_ev_heap(readHandle: &ReadHandle<String, Box<State>>) -> BinaryHeap<State> {
     let mut heap = BinaryHeap::<State>::with_capacity(HEAP_CAPACITY);
 
+    let now = Instant::now();
+
     for (key, value) in  &readHandle.read().unwrap() {
         match value.get_one() {
             None => println!("Oops get_one"),
             Some(val) => {
                 let unboxedValue = &(*val);
 
-                if heap.len() > HEAP_CAPACITY {
-                    heap.pop();
-                }
                 let keystring = key.to_string();
                 let a = keystring.as_bytes();
                 let b: Vec<u8> = a.iter().cloned().collect();
                 heap.push(State { id: Box::new(b), score: unboxedValue.score });
+
+                if heap.len() > HEAP_CAPACITY {
+                    heap.pop();
+                }
             },
         }        
     }
-    heap.pop();
 
-    println!("Size of heap: {}", heap.len());
+    println!("Size of heap: {}, Heap generation time: {}", heap.len(), now.elapsed().as_millis());
     return heap
 }
 
@@ -115,39 +116,42 @@ fn main() {
 
     let now = Instant::now();
 
-    // use std::sync::mpsc;
+    use std::sync::mpsc;
     
-    // let (tx, rx) = mpsc::channel();
+    let (tx, rx) = mpsc::channel();
 
-    // for i in 0..NTHREADS {
-    //     // Spin up another thread
-    //     let (reader, _) = &maps[i];
-    //     let cloned_reader = reader.clone();
-
-    //     thread::spawn(move || {
-    //          let heap = gen_ev_heap(&cloned_reader);
-    //          tx.send(heap).unwrap();
-    //     });
-    // }
-
-    use std::str;
-
-    // let received = rx.recv().unwrap();
-    let mut heaps = Vec::new();
     for i in 0..NTHREADS {
         // Spin up another thread
         let (reader, _) = &maps[i as usize];
         let cloned_reader = reader.clone();
 
-        let heap = gen_ev_heap(&cloned_reader);
-        heaps.push(heap);
+        let tx = tx.clone();
+        thread::spawn(move || {
+             let heap = gen_ev_heap(&cloned_reader);
+             println!("Sending heap {}...", i);
+             tx.send(heap).unwrap();
+        });
     }
 
-    for heap in heaps {
-        for x in heap {
-            println!("{} - {}", str::from_utf8(&*x.id).unwrap(), x.score);
+    let mut consolidated_heap = BinaryHeap::<State>::with_capacity(HEAP_CAPACITY);
+
+    for i in 0..NTHREADS  {
+        let received = rx.recv().unwrap();
+        println!("Received {} items for heap {}", received.len(), i);
+        for x in received {
+            consolidated_heap.push(State { id: x.id, score: x.score });
+            if consolidated_heap.len() > HEAP_CAPACITY {
+                consolidated_heap.pop();
+            }
         }
     }
+
+    for x in consolidated_heap {
+        println!("Response {} - {}", std::str::from_utf8(&*x.id).unwrap(), x.score);
+    }
+
+
+
 
     println!("Scanning memory with capacity {}...complete", SIZE);
     println!("Took time: {} ms", now.elapsed().as_millis());
